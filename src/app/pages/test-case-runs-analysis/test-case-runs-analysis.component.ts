4c4b0ded4183723bf63453ad7a9879ca7a05148d
Abdelwaheb AMMAR <aammar@ige-xao.local>
2022-01-19 17:27:44 +0100

    Feat/V1.0.0-1/TestCaseRunsAnalysis/Creation DataGrid + connexion API

--- src/app/pages/test-case-runs-analysis/test-case-runs-analysis.component.ts
+++ src/app/pages/test-case-runs-analysis/test-case-runs-analysis.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CustomerService } from 'src/app/shared/services/customer.service';
 
 @Component({
   selector: 'app-test-case-runs-analysis',
   templateUrl: './test-case-runs-analysis.component.html',
   styleUrls: ['./test-case-runs-analysis.component.scss']
 })
-export class TestCaseRunsAnalysisComponent implements OnInit {
+export class TestCaseRunsAnalysisComponent implements OnInit,OnDestroy {
 
-  constructor() { }
+  customers: any[];
+  customersSubscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(private customerService: CustomerService) {
+    this.customers = [];
+    this.customersSubscription = new Subscription();
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
+ 
+  async ngOnInit(): Promise<void> {
+    await this.customerService.getAllCustomers();
+    this.customersSubscription = this.customerService.customersSubject.subscribe(data => {
+      console.log(data)
+      this.customers = data.issues;
+    });
+    this.customerService.emitCustomers();
   }
 
 }
