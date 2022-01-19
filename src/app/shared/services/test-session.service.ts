c6f6207004c2ddfd6d62057398d68f735472756b
Ahmed MOALLA <amoalla@ige-xao.local>
2022-01-19 16:47:55 +0100

    first step : datagrid+apicall

---
+++ src/app/shared/services/test-session.service.ts
@@ -0,0 +1,33 @@
+import { environment } from '../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class C {
+
+  apiUrl = environment.apiUrl;
+
+  private testSessions: any;
+  testSessionsSubject : Subject<any>;
+
+  constructor(private httpClient: HttpClient) {
+    this.testSessions = [];
+    this.testSessionsSubject = new Subject<any>();
+
+  }
+
+  emitTestSessions(): void{
+    this.testSessionsSubject.next(this.testSessions);
+  }
+
+
+  async getTestSessions(): Promise<void>{
+    this.testSessions = await this.httpClient.get<any>(this.apiUrl+'testSession/read.php?idProd=0&Version=0&date=2021-01&ListStatus=1&from=table&index=0').toPromise();
+    this.emitTestSessions();
+  }
+
+}
